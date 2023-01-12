d257aebe0bbeff16d58b54e5ad6eda6ac27fa943
alvarophylipe <alvarophylipe@gmail.com>
2023-01-12 18:19:34 -0300

    Update linear model

--- simple-linear-regression/linear_model.py
+++ simple-linear-regression/linear_model.py
@@ -8,15 +8,25 @@ class LinearRegression:
         self.b:np.array
         self.n:np.int16
     
+    
     def fit(self, x, y):
         self.x = np.array(x)
         self.y = np.array(y)
         self.n = len(self.x)
         #coeficiente angular
-        self.b = (self.n * np.sum(self.x * self.y) - np.sum(self.x) * np.sum(self.y)) / (self.n * np.sum(self.x ** 2) - (np.sum(self.x) ** 2))
+        self.b = self.coef_()
         #intercepto
-        self.a = (np.sum(self.y) - self.b * np.sum(self.x)) / self.n
+        self.a = self.intercept_()
         print('Sucess')
+    
     
     def predict(self, x):
         return self.a + self.b * np.array(x)
+    
+    
+    def coef_(self):
+        return (self.n * np.sum(self.x * self.y) - np.sum(self.x) * np.sum(self.y)) / (self.n * np.sum(self.x ** 2) - (np.sum(self.x) ** 2))
+    
+    
+    def intercept_(self):
+        return (np.sum(self.y) - self.b * np.sum(self.x)) / self.n
